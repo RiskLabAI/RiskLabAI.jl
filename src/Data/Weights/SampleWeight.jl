406aa9a7084b220ec6f8a39a674915a070e4bb2f
Hamid Arian <a1r7i2a9n@gmail.com>
2022-08-29 00:33:07 -0400

    minor changes on sample weights

--- src/Data/Weights/SampleWeight.jl
+++ src/Data/Weights/SampleWeight.jl
@@ -3,21 +3,21 @@ function: expand label tO incorporate meta-labeling
 reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
 methodology: 51
 """
-function ConcurrencyEvents(closeIndex, # DataFrame that has events
-                            timestamp, # DateFrame that has return and label of each period
+function concurrencyEvents(closeIndex, # DataFrame that has events
+                            timeStamp, # DateFrame that has return and label of each period
                             molecule) # index that function must apply on it
 
-    eventsfiltered = filter(row -> row[:date] in molecule, timestamp) # filter events respect to molecule
-    starttime = eventsfiltered.date[1]
-    endtime = maximum(eventsfiltered.timestamp)
+    eventsFiltered = filter(row -> row[:date] in molecule, timeStamp) # filter events respect to molecule
+    starttime = eventsFiltered.date[1]
+    endtime = maximum(eventsFiltered.timeStamp)
     concurrencyindex = closeIndex[(closeIndex .>= starttime) .& (closeIndex .<= endtime)]
     concurrency = DataFrame(date=concurrencyindex , concurrency = zeros(size(concurrencyindex)[1])) 
     # create dataframe that contain number of concurrent label for each events 
 
-    for (i, idx) in enumerate(eventsfiltered.date)
+    for (i, idx) in enumerate(eventsFiltered.date)
          
         startIndex  = concurrency.date .>= idx # store events that before idx
-        endIndex = concurrency.date .<= eventsfiltered.timestamp[i] # store events that end after idx
+        endIndex = concurrency.date .<= eventsFiltered.timeStamp[i] # store events that end after idx
         selectedIndex = startIndex .& endIndex
         concurrency.concurrency[selectedIndex] .+= 1
     end
@@ -25,19 +25,19 @@ function ConcurrencyEvents(closeIndex, # DataFrame that has events
 end
 
 """
-function: SampleWeight with triple barrier
+function: sampleWeight with triple barrier
 reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
 methodology: 51
 """
-function SampleWeight(timestamp, # DataFrame of events start and end for labelling 
-                      ConcurrencyEvents, # Data frame of concurrent events for each events 
-                        molecule)# index that function must apply on it
-    eventsfiltered = filter(row -> row[:date] in molecule,timestamp) # filter timestamp by molecule
+function sampleWeight(timeStamp, # DataFrame of events start and end for labelling 
+                      concurrencyEvents, # Data frame of concurrent events for each events 
+                      molecule)# index that function must apply on it
+    eventsFiltered = filter(row -> row[:date] in molecule,timeStamp) # filter timeStamp by molecule
     weight = DataFrame(date = molecule,weight = zeros(length(molecule))) # create DataFrame for result 
     for i in 1:size(weight)[1]
-        starttime, endtime = eventsfiltered.date[i] , eventsfiltered.timestamp[i] #initial starting Time and ending time 
-        ConcurrencyEventsForSpecificTime = ConcurrencyEvents[(ConcurrencyEvents.date .>= starttime) .& (ConcurrencyEvents.date .<= endtime),"concurrency"] # compute concurrency for this time horizon
-        weight[i,"weight"] = mean(1 ./ ConcurrencyEventsForSpecificTime) #compute weight
+        starttime, endtime = eventsFiltered.date[i] , eventsFiltered.timeStamp[i] #initial starting Time and ending time 
+        concurrencyEventsForSpecificTime = concurrencyEvents[(concurrencyEvents.date .>= starttime) .& (concurrencyEvents.date .<= endtime),"concurrency"] # compute concurrency for this time horizon
+        weight[i,"weight"] = mean(1 ./ concurrencyEventsForSpecificTime) #compute weight
     end
     return weight
 end
@@ -47,9 +47,9 @@ reference: De Prado, M. (2018) Advances in financial machine learning. John Wile
 methodology: 51
 """
 function IndexMatrix(barIndex, #index of all data 
-                     timestamp) #times of events contain starting and ending time 
-    indexMatrix = zeros((size(barIndex)[1] , size(timestamp)[1])) #creat evetnt matrix that show index is time horizon or not 
-    for (j,(t0,t1)) in enumerate(timestamp)
+                     timeStamp) #times of events contain starting and ending time 
+    indexMatrix = zeros((size(barIndex)[1] , size(timeStamp)[1])) #creat evetnt matrix that show index is time horizon or not 
+    for (j,(t0,t1)) in enumerate(timeStamp)
         Indicator = [(i<=t1 && i>= t0 ) ? 1 : 0 for i in barIndex] #if index is in events put 1 in its entry 
         indexMatrix[!,j] = Indicator
     end
@@ -104,19 +104,19 @@ end
     reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
     methodology: 51
 """
-function SampleWeight(timestamp, # dataframe for events 
-                      ConcurrencyEvents,# dataframe that contain number of concurrent evnets for each evnetst 
+function sampleWeight(timeStamp, # dataframe for events 
+                      concurrencyEvents,# dataframe that contain number of concurrent evnets for each evnetst 
                       returns,#data frame that contains returns
                       molecule) # molecule
-    eventsfiltered = filter(row -> row[:date] in molecule,timestamp) #filter timestamp with molecule
+    eventsFiltered = filter(row -> row[:date] in molecule,timeStamp) #filter timeStamp with molecule
     weight = DataFrame(date = molecule,weight = zeros(length(molecule))) #create weight dataframe for results 
     priceReturn = copy(returns)
     priceReturn.returns = log.(returns.returns .+ 1) # compute log returns
     for i in 1:size(weight)[1]
-        starttime, endtime = eventsfiltered.date[i] , eventsfiltered.timestamp[i] #initial statring time and endtime time of event 
-        ConcurrencyEventsForSpecificTime = ConcurrencyEvents[(ConcurrencyEvents.date .>= starttime) .& (ConcurrencyEvents.date .<= endtime),:] # compute concurrency for event
+        starttime, endtime = eventsFiltered.date[i] , eventsFiltered.timeStamp[i] #initial statring time and endtime time of event 
+        concurrencyEventsForSpecificTime = concurrencyEvents[(concurrencyEvents.date .>= starttime) .& (concurrencyEvents.date .<= endtime),:] # compute concurrency for event
         ReturnForSpecificTime = priceReturn[(priceReturn.date .>= starttime) .& (priceReturn.date .<= endtime),:returns] #select subrow of return that occure in this event 
-        weight.weight[i] = sum(ReturnForSpecificTime ./ ConcurrencyEventsForSpecificTime.concurrency) #comput weight
+        weight.weight[i] = sum(ReturnForSpecificTime ./ concurrencyEventsForSpecificTime.concurrency) #comput weight
     end
     weight.weight = abs.(weight.weight) #weight must be positive !
     return weight
