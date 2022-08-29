cce5d4ff2b96926bc2a1464df9df58a6e58e0564
Hamid Arian <a1r7i2a9n@gmail.com>
2022-08-29 00:35:05 -0400

    minor changes on sample weights

--- test.jl
+++ test.jl
@@ -2,7 +2,7 @@ using PkgTemplates
 
 t = Template(; 
     user="hamid-arian",
-    dir="~/Code/PkgLocation/",
+    dir="~/PkgLocation/",
     julia=v"1.8",
     plugins=[
         Git(; manifest=true, ssh=true),
