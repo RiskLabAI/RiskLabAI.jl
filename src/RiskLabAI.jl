700477c275e2d7535752cc5a4e5498f380c858c3
hamid-arian <arian@sharif.edu>
2022-09-09 00:39:08 +0430

    doc change

--- src/RiskLabAI.jl
+++ src/RiskLabAI.jl
@@ -10,12 +10,11 @@ export
     GenerateSignal,
 
 
-    #
-    sampleWeight
+    
 
  include("Backtests/ProbabilityOfBacktestOverfitting.jl")
  include("BetSize/BetSizing.jl")
- include("Data/Weights/SampleWeight.jl")
+
 
 end
 
