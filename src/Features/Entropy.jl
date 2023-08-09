e6c839502e381ac8526ec0ae812fa9189e763ad7
Seyed Alireza Mousavizade <107984049+salireza-mousavizade@users.noreply.github.com>
2023-08-10 00:07:30 +0330

    reformat code

--- src/Features/Entropy.jl
+++ src/Features/Entropy.jl
@@ -1,160 +1,165 @@
 module Entropy
 
-export shannonEntropy, lempleZivEntropy, probabilityMassFunction, plugInEntropyEstimator, kontoyiannisEntorpy
+export shannon_entropy, lemple_ziv_entropy, probability_mass_function, plug_in_entropy_estimator, kontoyiannis_entropy
 
 """
-    function: Shannon Entropy 
-    reference: De Prado, M. (18) Advances in Financial Machine Learning
-    methodology: page 263 SHANNON’S ENTROPY section
-"""
-function shannonEntropy(
-    message::String # input encoded message
-)::Float64
+    Calculate Shannon Entropy
 
-    charToCount = Dict()
-    entropy = 0
+    Parameters:
+    - message::String: Input encoded message.
+
+    Returns:
+    - entropy::Float64: Shannon Entropy score.
+"""
+function shannon_entropy(message::String)::Float64
+    char_to_count = Dict()
+    entropy = 0.0
+    message_length = length(message)
+
     for character ∈ message
         try
-            charToCount[character] += 1
+            char_to_count[character] += 1
         catch
-            charToCount[character] = 1
+            char_to_count[character] = 1
         end
     end
 
-    messageLength = length(message)
-    for count ∈ values(charToCount)
-        frequency = count / messageLength
+    for count ∈ values(char_to_count)
+        frequency = count / message_length
         entropy -= frequency * log2(frequency)
     end
 
-    entropy
+    return entropy
 end
 
 """
-    function: A LIBRARY BUILT USING THE LZ ALGORITHM
-    reference: De Prado, M. (18) Advances in Financial Machine Learning
-    methodology: page 266 LEMPEL-ZIV ESTIMATORS section
-"""
-function lempleZivEntropy(
-    message::String # input encoded message
-)::Float64
+    Calculate Lemple-Ziv Entropy
 
+    Parameters:
+    - message::String: Input encoded message.
+
+    Returns:
+    - entropy::Float64: Lemple-Ziv Entropy score.
+"""
+function lemple_ziv_entropy(message::String)::Float64
     i, library = 1, Set{String}([message[1] |> string])
-    messageLength = length(message)
-    while i < messageLength
-        lastJValue = messageLength - 1
-        for j ∈ i:messageLength-1
-            message_ = message[i+1:j+1]
+    message_length = length(message)
+
+    while i < message_length
+        last_j_value = message_length - 1
+        for j ∈ i:message_length - 1
+            message_ = message[i + 1:j + 1]
             if message_ ∉ library
                 push!(library, message_)
-                lastJValue = j
+                last_j_value = j
                 break
             end
         end
-        i = lastJValue + 1
+        i = last_j_value + 1
     end
 
-
-    length(library) / length(message)
+    return length(library) / message_length
 end
 
 """
-    function: Calculate probability mass function (PMF)
-    reference: De Prado, M. (18) Advances in Financial Machine Learning
-    methodology: page 265 THE PLUG-IN (OR MAXIMUM LIKELIHOOD) ESTIMATOR section
+    Calculate probability mass function (PMF)
+
+    Parameters:
+    - message::String: Input encoded message.
+    - approximate_word_length::Int: Approximation of word length.
+
+    Returns:
+    - pmf::Dict: Probability mass function.
 """
-function probabilityMassFunction(
-    message::String, # input encoded message
-    approximateWordLength::Int # approximation of word length
-)::Dict
-
+function probability_mass_function(message::String, approximate_word_length::Int)::Dict
     library = Dict()
+    message_length = length(message)
 
-    messageLength = length(message)
-    for index ∈ approximateWordLength:messageLength-1
-        message_ = message[index-approximateWordLength+1:index] 
+    for index ∈ approximate_word_length:message_length - 1
+        message_ = message[index - approximate_word_length + 1:index]
 
         if message_ ∉ keys(library)
-            library[message_] = [index - approximateWordLength + 1]
+            library[message_] = [index - approximate_word_length + 1]
         else
-            append!(library[message_], index - approximateWordLength + 1)
+            append!(library[message_], index - approximate_word_length + 1)
         end
     end
 
-    denominator = (messageLength - approximateWordLength) |> float
-    probabilityMassFunction = Dict([
-        key => length(library[key]) / denominator for key ∈ keys(library)
-    ])
-
-    probabilityMassFunction
+    denominator = (message_length - approximate_word_length) |> float
+    pmf = Dict([key => length(library[key]) / denominator for key ∈ keys(library)])
+    return pmf
 end
 
 """
-    function: Plug-in Entropy Estimator Implementation
-    reference: De Prado, M. (18) Advances in Financial Machine Learning
-    methodology: page 265 THE PLUG-IN (OR MAXIMUM LIKELIHOOD) ESTIMATOR section
-"""
-function plugInEntropyEstimator(
-    message::String; # input encoded message
-    approximateWordLength::Int=1 # approximation of word length
-)::Float64
+    Calculate Plug-in Entropy Estimator
 
-    pmf = probabilityMassFunction(message, approximateWordLength)
-    plugInEntropyEstimator = -sum([pmf[key] * log2(pmf[key]) for key ∈ keys(pmf)]) / approximateWordLength
-    plugInEntropyEstimator
+    Parameters:
+    - message::String: Input encoded message.
+    - approximate_word_length::Int: Approximation of word length.
+
+    Returns:
+    - entropy::Float64: Plug-in Entropy Estimator score.
+"""
+function plug_in_entropy_estimator(message::String, approximate_word_length::Int=1)::Float64
+    pmf = probability_mass_function(message, approximate_word_length)
+    plug_in_entropy_estimator = -sum([pmf[key] * log2(pmf[key]) for key ∈ keys(pmf)]) / approximate_word_length
+    return plug_in_entropy_estimator
 end
 
 """
-    function: COMPUTES THE LENGTH OF THE LONGEST MATCH
-    reference: De Prado, M. (18) Advances in Financial Machine Learning
-    methodology: page 267 LEMPEL-ZIV ESTIMATORS section
-"""
-function longestMatchLength(
-    message::String,
-    i::Int,
-    n::Int
-)::Tuple{Int, String}
+    Calculate the length of the longest match
 
-    subString = ""
+    Parameters:
+    - message::String: Input encoded message.
+    - i::Int: Starting index for the search.
+    - n::Int: Length of the expanding window.
+
+    Returns:
+    - match_length::Tuple{Int, String}: Match length and matched substring.
+"""
+function longest_match_length(message::String, i::Int, n::Int)::Tuple{Int, String}
+    sub_string = ""
     for l ∈ 1:n
-        message1 = message[i:i+l]
-        for j ∈ i-n+1:i
-            message0 = message[j:j+l]
+        message1 = message[i:i + l]
+        for j ∈ i - n + 1:i
+            message0 = message[j:j + l]
             if message1 == message0
-                subString = message1
-                break # search for higher l.
+                sub_string = message1
+                break  # search for higher l.
             end
         end
     end
 
-    return (length(subString) + 1, subString) # matched length + 1 
+    return (length(sub_string) + 1, sub_string)  # matched length + 1
 end
 
 """
-    function: IMPLEMENTATION OF ALGORITHMS DISCUSSED IN GAO ET AL.
-    reference: De Prado, M. (18) Advances in Financial Machine Learning
-    methodology: page 268 LEMPEL-ZIV ESTIMATORS section
-"""
-function kontoyiannisEntorpy(
-    message::String; # input encoded message
-    window::Int=0, # length of expanding window 
-)::Float64
+    Calculate Kontoyiannis Entropy
 
+    Parameters:
+    - message::String: Input encoded message.
+    - window::Int: Length of the expanding window.
+
+    Returns:
+    - entropy::Float64: Kontoyiannis Entropy score.
+"""
+function kontoyiannis_entropy(message::String, window::Int=0)::Float64
     output = Dict("num" => 0, "sum" => 0, "subString" => [])
+
     if window === nothing
-        points = 2:length(message)÷2+1
+        points = 2:length(message) ÷ 2 + 1
     else
         window = min(window, length(message) ÷ 2)
-        points = window+1:length(message)-window+1
+        points = window + 1:length(message) - window + 1
     end
 
     for i ∈ points
         if window === nothing
-            (l, message_) = longestMatchLength(message, i, i)
-            output["sum"] += log2(i) / l # to avoid Doeblin condition
+            (l, message_) = longest_match_length(message, i, i)
+            output["sum"] += log2(i) / l  # to avoid Doeblin condition
         else
-            (l, message_) = longestMatchLength(message, i, window)
-            output["sum"] += log2(window) / l # to avoid Doeblin condition
+            (l, message_) = longest_match_length(message, i, window)
+            output["sum"] += log2(window) / l  # to avoid Doeblin condition
         end
 
         append!(output["subString"], message_)
@@ -162,9 +167,8 @@ function kontoyiannisEntorpy(
     end
 
     output["h"] = output["sum"] / output["num"]
-    output["r"] = 1 - output["h"] / log2(length(message)) 
-
-    output["h"] 
+    output["r"] = 1 - output["h"] / log2(length(message))
+    return output["h"]
 end
 
 end
--- src/Features/MeanDecreaseAccuracy.jl
+++ src/Features/MeanDecreaseAccuracy.jl
@@ -1,6 +1,5 @@
 using DataFrames
 using DataFramesMeta
-
 using PyCall
 using Statistics
 using PlotlyJS
@@ -13,57 +12,62 @@ using Random
 @pyimport sklearn.datasets as Datasets
 @pyimport sklearn.model_selection as ModelSelection
 
+"""
+    Calculate feature importance using Mean-Decrease Accuracy (MDA) method.
 
+    Parameters:
+    - classifier: Classifier for fit and prediction.
+    - X::DataFrame: Features matrix.
+    - y::DataFrame: Labels vector.
+    - nSplits::Int64: Number of cross-validation folds.
+    - scoreSampleWeights::Union{Vector, Nothing}: Sample weights for score step.
+    - trainSampleWeights::Union{Vector, Nothing}: Sample weights for train step.
+
+    Returns:
+    - DataFrame: DataFrame with feature importance scores.
 """
-    function: Implementation of MDA
-    reference: De Prado, M. (2020) MACHINE LEARNING FOR ASSET MANAGERS
-    methodology: page 82 Mean-Decrease Accuracy section snippet 6.3 (snippet 8.3 2018)
-"""
-function featureImportanceMDA(
-    classifier, # classifier for fit and prediction
-    X::DataFrame, # features matrix
-    y::DataFrame, # labels vector
-    nSplits::Int64; # cross-validation n folds
-    scoreSampleWeights::Union{Vector, Nothing}=nothing, # sample weights for score step
-    trainSampleWeights::Union{Vector, Nothing}=nothing, # sample weights for train step 
+function feature_importance_mda(
+    classifier, X::DataFrame, y::DataFrame, nSplits::Int64;
+    scoreSampleWeights::Union{Vector, Nothing}=nothing,
+    trainSampleWeights::Union{Vector, Nothing}=nothing
 )::DataFrame
 
-    trainSampleWeights = isnothing(trainSampleWeights) ? ones(size(X)[1]) : trainSampleWeights
-    scoreSampleWeights = isnothing(scoreSampleWeights) ? ones(size(X)[1]) : scoreSampleWeights
+    trainSampleWeights = isnothing(trainSampleWeights) ? ones(size(X, 1)) : trainSampleWeights
+    scoreSampleWeights = isnothing(scoreSampleWeights) ? ones(size(X, 1)) : scoreSampleWeights
 
     cvGenerator = ModelSelection.KFold(n_splits=nSplits)    
     score0, score1 = DataFrame("value" => zeros(nSplits)), DataFrame([name => zeros(nSplits) for name in names(X)])
 
-    for (i, (train, test)) ∈ enumerate(cvGenerator.split(X |> Matrix))
+    for (i, (train, test)) in enumerate(cvGenerator.split(Matrix(X)))
         println("fold $(i) start ...")
 
-        train .+= 1 # Python indexing starts at 0
-        test .+= 1 # Python indexing starts at 0
+        train .+= 1  # Python indexing starts at 0
+        test .+= 1   # Python indexing starts at 0
 
         X0, y0, sampleWeights0 = X[train, :], y[train, :], trainSampleWeights[train]
         X1, y1, sampleWeights1 = X[test, :], y[test, :], scoreSampleWeights[test]
-            
-        fit = classifier.fit(X0 |> Matrix, y0 |> Matrix |> vec, sample_weight=sampleWeights0)
-        
-        predictionProbability = fit.predict_proba(X1 |> Matrix)
 
-        score0[i, 1] = -Metrics.log_loss(y1 |> Matrix, predictionProbability, sample_weight=sampleWeights1, labels=classifier.classes_)        
-        
-        for j ∈ names(X)
-            X1_ = deepcopy(X1) 
-            X1_[!,  j] = shuffle(X1_[!, j])
+        fit = classifier.fit(Matrix(X0), vec(Matrix(y0)), sample_weight=sampleWeights0)
 
-            predictionProbability = fit.predict_proba(X1_ |> Matrix)
-            log_loss = Metrics.log_loss(y1 |> Matrix, predictionProbability, labels=classifier.classes_)
+        predictionProbability = fit.predict_proba(Matrix(X1))
+
+        score0[i, 1] = -Metrics.log_loss(Matrix(y1), predictionProbability, sample_weight=sampleWeights1, labels=classifier.classes_)
+
+        for j in names(X)
+            X1_ = deepcopy(X1)
+            X1_[!, j] = shuffle(X1_[!, j])
+
+            predictionProbability = fit.predict_proba(Matrix(X1_))
+            log_loss = Metrics.log_loss(Matrix(y1), predictionProbability, labels=classifier.classes_)
             score1[i, j] = -log_loss
         end
     end
 
-    importances = DataFrame(.-Matrix(score1) .+ Matrix(score0), names(score1))
-    importances = importances ./ .-score1
+    importances = DataFrame(-Matrix(score1) .+ Matrix(score0), names(score1))
+    importances = importances ./ (-score1)
 
     means = mean.(eachcol(importances))
-    stds = std.(eachcol(importances)) .* (nrow(importances)^(-0.5)) 
+    stds = std.(eachcol(importances)) .* (size(importances, 1)^(-0.5))
 
-    return DataFrame("FeatureName"=> names(X),"Mean"=>means, "SandardDeviation"=>stds)
-end 
+    return DataFrame("FeatureName" => names(X), "Mean" => means, "StandardDeviation" => stds)
+end
