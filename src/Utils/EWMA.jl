eb666794a7187e653b947bb50ae63101c842cd2d
Seyed Alireza Mousavizade <107984049+salireza-mousavizade@users.noreply.github.com>
2023-08-10 00:24:41 +0330

    snake case to lower camel case

--- src/Utils/EWMA.jl
+++ src/Utils/EWMA.jl
@@ -3,21 +3,21 @@ Compute the exponentially weighted moving average (EWMA), EWMA variance, and EWM
 
 Reference: https://stackoverflow.com/questions/40754262/pandas-ewm-std-calculation
 """
-function ewma(arr_in::Vector, window::Int)
-    arr_length = length(arr_in)
-    ewma_arr = zeros(arr_length)
+function ewma(arrIn::Vector, window::Int)
+    arrLength = length(arrIn)
+    ewmaArr = zeros(arrLength)
 
     alpha = 2 / (window + 1)
     weight = 1
 
-    ewma_old = arr_in[1]
-    ewma_arr[1] = ewma_old
+    ewmaOld = arrIn[1]
+    ewmaArr[1] = ewmaOld
 
-    for i in 2:arr_length
+    for i in 2:arrLength
         weight += (1 - alpha) ^ i
-        ewma_old = ewma_old * (1 - alpha) + arr_in[i]
-        ewma_arr[i] = ewma_old / weight
+        ewmaOld = ewmaOld * (1 - alpha) + arrIn[i]
+        ewmaArr[i] = ewmaOld / weight
     end
 
-    ewma_arr
+    ewmaArr
 end
