0e84ce2e5e9353c2e3a8354cbb3781e742e1be91
Seyed Alireza Mousavizade <107984049+salireza-mousavizade@users.noreply.github.com>
2023-08-09 23:08:42 +0330

    reformat code

--- src/Backtests/ProbabilityOfBacktestOverfitting.jl
+++ src/Backtests/ProbabilityOfBacktestOverfitting.jl
@@ -1,68 +1,66 @@
 """
-    function: Selecting row from given prartion
-    reference: -
-    methodology: -
+Selects rows from a given partition.
+
+:param partition: Index of submatrix.
+:param sub_matrix_size: Submatrix size.
+:return: Array of selected indexes.
 """
-function selectedRow(
-        partition, # index of submatrix 
-        subMatrixSize # sub matrix size 
-)    
-
-    array = [] # create array for storing indexes 
+function selectedRow(partition, subMatrixSize)
+    array = []
+    
     for p in partition
-        start = (p-1)*subMatrixSize + 1 # inital starting point of partition
-        ends = p*subMatrixSize # initial ending point of partition
-        append!(array,[start:ends]) # append this indexes to array 
+        start = (p - 1) * subMatrixSize + 1
+        ends = p * subMatrixSize
+        append!(array, [start:ends])
     end
-
-    return (vcat(array...))
-
+    
+    return vcat(array...)
 end
 
 """
-    function: Computing Probability Of BacktestOverfitting
-    reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
-    methodology: p.156
-"""
-function ProbabilityOfBacktestOverfitting(
-        matrixData, # matrix of T×N for T observation on N strategy 
-        nPartions, # Number of Partition (must be even)
-        metric;    # metric function for evaluate strategy
-        RiskFreeReturn  = 0 # RiskFreeReturn for calculate sharp ratio
-) 
+Computes the Probability Of Backtest Overfitting.
 
-    if nPartions % 2 == 1 #check number of partition is even or not 
-        print("Number of Partition must be even") # if S is odd then stop algorithm and ask number of partition must be even 
+:param matrixData: Matrix of T×N for T observations on N strategies.
+:param nPartitions: Number of partitions (must be even).
+:param metric: Metric function for evaluating strategy.
+:param riskFreeReturn: Risk-free return for calculating Sharpe ratio.
+:return: Tuple containing Probability Of Backtest Overfitting and an array of logit values.
+"""
+function probabilityOfBacktestOverfitting(matrixData, nPartitions, metric;
+                                          riskFreeReturn=0.0)
+    if nPartitions % 2 == 1
+        println("Number of partitions must be even")
         return
     end
     
-    nObservation,nStrategy = size(matrixData) # inital number of observation and number of strategy
-    exceed = nObservation % nPartions 
-    nObservation  -= exceed # minus exceed from number of observation because we want nPartions|nObservation
-    Matrix = matrixData[1:nObservation,:] #select first nObservation  of data
-    subMatrixSize = Int(nObservation // nPartions ) # calculate size of each partition size 
-    partition = collect(combinations(1:nPartions,Int(nPartions//2))) # compute all combinations of size nPartions/2
-    pboNum = Threads.Atomic{Int}(0) # initial Atomic pboNum with zero
-    ResultInThreads = [[] for i in 1:Threads.nthreads()]
+    nObservation, nStrategy = size(matrixData)
+    exceed = nObservation % nPartitions
+    nObservation -= exceed
+    matrix = matrixData[1:nObservation, :]
+    subMatrixSize = Int(nObservation // nPartitions)
+    partition = collect(combinations(1:nPartitions, Int(nPartitions / 2)))
+    pboNum = Threads.Atomic{Int}(0)
+    resultInThreads = [[] for _ in 1:Threads.nthreads()]
     
     Threads.@threads for p in partition
-        index = [i for i in 1:nObservation] # create array of all index 
-        selectedrow = SelectedRow(p,subMatrixSize) # select row of this partition
-        trainData = Matrix[selectedrow,:] #inital train data 
-        evaluate = [metric(trainData[:,i],RiskFreeReturn) for i in 1:nStrategy] # evaluate strategy on train data 
-        bestStrategy = sortperm(evaluate)[nStrategy] # calculate best strategy in train data and save its index 
-        testindex = index .∉ Ref(selectedrow) #inital test data index
-        testData = Matrix[testindex,:] # inital test data 
-        evaluate = [metric(testData[:,i],RiskFreeReturn) for i in 1:nStrategy] # evaluate strategy on test data 
-        testindex = findall(x -> x == bestStrategy,sortperm(evaluate))[1] # find plcae of bestStrategy in sorted performance on test data 
-        wbar = Float64(testindex) / (nStrategy+1) # calculate wbar
-        logit = log(wbar/(1-wbar)) # calculate logit 
-        if logit <= 0.0 # check if logit is less than zero 
-            Threads.atomic_add!(pboNum, 1) # if logit is less than zero it means that it is overfitting case 
+        index = 1:nObservation
+        selectedRowIndices = selectedRow(p, subMatrixSize)
+        trainData = matrix[selectedRowIndices, :]
+        evaluate = [metric(trainData[:, i], riskFreeReturn) for i in 1:nStrategy]
+        bestStrategy = argmax(evaluate)
+        testIndex = .! in(selectedRowIndices)
+        testData = matrix[testIndex, :]
+        evaluate = [metric(testData[:, i], riskFreeReturn) for i in 1:nStrategy]
+        testIndex = findfirst(x -> x == bestStrategy, argmax(evaluate))
+        wbar = Float64(testIndex) / (nStrategy + 1)
+        logit = log(wbar / (1 - wbar))
+        
+        if logit <= 0.0
+            Threads.atomic_add!(pboNum, 1)
         end
-        append!(ResultInThreads[Threads.threadid()],logit)
+        
+        push!(resultInThreads[Threads.threadid()], logit)
     end
-
-    return pboNum[] / length(partition),vcat(ResultInThreads...) # calculate  Probability Of Backtest Overfitting
-
+    
+    return pboNum[] / length(partition), vcat(resultInThreads...)
 end
--- src/Backtests/StrategyRisk.jl
+++ src/Backtests/StrategyRisk.jl
@@ -1,143 +1,124 @@
-# todo: fix function signatures
-
-using Distributions
-using Random
-using Statistics
-using SymPy
-
 """
-	function: targets a Sharpe ratio as a function of the number of bets
-	reference: De Prado, M. (2018) Advances in financial machine learning.
-	methodology: page 213, snippet 15.1
+Function to calculate the Sharpe ratio trials.
+
+:param p: Probability of success.
+:param nRun: Number of runs.
+:return: Tuple containing mean, standard deviation, and Sharpe ratio.
 """
-function sharpeRatioTrials(
-		p::Float64, # probability of success
-		nRun::Int64 # number of runs
-)::Tuple{Float64, Float64, Float64} 
-
-    result = [] # initial results list
-
+function sharpeRatioTrials(p::Float64, nRun::Int64)::Tuple{Float64, Float64, Float64}
+    result = []
+    
     for i in 1:nRun
-        b = Binomial(1, p) # binomial distribution with probability p
-        random = rand(b, 1) # generate random number with binomial distribution b
-
-        if random[1] == 1 # find if the generated number is 1
-            x = 1 # define x as 1
-        else 
-            x = -1 # define x as -1
+        b = Binomial(1, p)
+        random = rand(b, 1)
+        
+        if random[1] == 1
+            x = 1
+        else
+            x = -1
         end
-
-        append!(result, [x]) # append result
+        
+        push!(result, [x])
     end
-
-    return (mean(result), std(result), mean(result)/std(result))
-end 
+    
+    return (mean(result), std(result), mean(result) / std(result))
+end
 
 """
-	function: uses the SymPy library for symbolic operations 
-	reference: De Prado, M. (2018) Advances in financial machine learning.
-	methodology: page 214, snippet 15.2
+Function to compute the target Sharpe ratio using symbolic operations.
+
+:return: Symbolic expression for the target Sharpe ratio.
 """
 function targetSharpeRatioSymbolic()
-
-    p,u,d = symbols("p u d") # Create symbols
-
-    m2 = p*u^2 + (1 - p)*d^2 # do symbolic operations
-    m1 = p*u + (1 - p)*d # do symbolic operations
-    v = m2 - m1^2 # do symbolic operations
-
-    factor(v) 
+    p, u, d = symbols("p u d")
+    
+    m2 = p * u^2 + (1 - p) * d^2
+    m1 = p * u + (1 - p) * d
+    v = m2 - m1^2
+    
+    return factor(v)
 end
 
 """
-function: computes implied precision 
-reference: De Prado, M. (2018) Advances in financial machine learning.
-methodology: page 214, snippet 15.3
+Function to compute implied precision.
+
+:param stopLoss: Stop loss threshold.
+:param profitTaking: Profit taking threshold.
+:param frequency: Number of bets per year.
+:param targetSharpeRatio: Target annual Sharpe ratio.
+:return: Implied precision.
 """
-function impliedPrecision(
-		stopLoss::Float64, # stop loss threshold
-		profitTaking::Float64, # profit taking threshold
-		frequency::Int64, # number of bets per year
-		targetSharpeRatio::Flaot64 # target annual Sharpe ratio
-)::Float64
-
-	a = (frequency + targetSharpeRatio^2)*(profitTaking - stopLoss)^2 # calculate the "a" parameter for the quadratic euation
-	b = (2*frequency*stopLoss - targetSharpeRatio^2*(profitTaking - stopLoss))*(profitTaking - stopLoss) # calculate the "b" parameter for the quadratic euation
-	c = frequency*stopLoss^2 # calculate the "c" parameter for the quadratic euation
-	precision = (-b + (b^2 - 4*a*c)^0.5)/(2*a) # solve the quadratic equation
-
-	return precision
+function impliedPrecision(stopLoss::Float64, profitTaking::Float64, frequency::Int64, targetSharpeRatio::Float64)::Float64
+    a = (frequency + targetSharpeRatio^2) * (profitTaking - stopLoss)^2
+    b = (2 * frequency * stopLoss - targetSharpeRatio^2 * (profitTaking - stopLoss)) * (profitTaking - stopLoss)
+    c = frequency * stopLoss^2
+    precision = (-b + sqrt(b^2 - 4 * a * c)) / (2 * a)
+    
+    return precision
 end
 
 """
-function: computes the number of bets/year needed to achieve a Sharpe ratio with a certain precision rate
-reference: De Prado, M. (2018) Advances in financial machine learning.
-methodology: page 215, snippet 15.4
-"""
-function binFrequency(stopLoss, # stop loss threshold
-					  profitTaking, # profit taking threshold
-					  precision, # precision rate p
-					  targetSharpeRatio) # target annual Sharpe ratio
+Function to compute the number of bets/year needed to achieve a Sharpe ratio with a certain precision rate.
 
-	frequency = (targetSharpeRatio*(profitTaking - stopLoss))^2*precision*(1 - precision) / ((profitTaking - stopLoss)*precision + stopLoss)^2  # calculate possible extraneous
-	
-	binSR(sl0, pt0, freq0, p0) = (((pt0 - sl0)*p0 + sl0)*freq0^0.5) / ((pt0 - sl0)*(p0*(1 - p0))^0.5) # Define Sharpe Ratio function
-	
-	if !isapprox(binSR(stopLoss, profitTaking, frequency, precision), targetSharpeRatio, atol=0.5) # check if it's near the target Sharpe Ratio
-		return nothing
+:param stopLoss: Stop loss threshold.
+:param profitTaking: Profit taking threshold.
+:param precision: Precision rate p.
+:param targetSharpeRatio: Target annual Sharpe ratio.
+:return: Number of bets per year.
+"""
+function binFrequency(stopLoss, profitTaking, precision, targetSharpeRatio)
+    frequency = (targetSharpeRatio * (profitTaking - stopLoss))^2 * precision * (1 - precision) /
+        ((profitTaking - stopLoss) * precision + stopLoss)^2
+    
+    binSR(sl0, pt0, freq0, p0) =
+        (((pt0 - sl0) * p0 + sl0) * freq0^0.5) / ((pt0 - sl0) * (p0 * (1 - p0))^0.5)
+    
+    if !isapprox(binSR(stopLoss, profitTaking, frequency, precision), targetSharpeRatio, atol = 0.5)
+        return nothing
     end
-
-	return frequency
+    
+    return frequency
 end
 
 """
-function: calculates the strategy risk in practice
-reference: De Prado, M. (2018) Advances in financial machine learning.
-methodology: page 215, snippet 15.4
+Function to calculate the strategy risk in practice.
+
+:param μ1: Mean of the first Gaussian distribution to generate bet outcomes.
+:param μ2: Mean of the second Gaussian distribution to generate bet outcomes.
+:param σ1: Standard deviation of the first Gaussian distribution to generate bet outcomes.
+:param σ2: Standard deviation of the second Gaussian distribution to generate bet outcomes.
+:param probability: Probability of success.
+:param nObs: Number of observations.
+:param frequency: Number of bets per year.
+:param targetSharpeRatio: Target annual Sharpe ratio.
+:return: Probability that the strategy may fail.
 """
-function mixGaussians(μ1, # mean of the first gaussian distribution to generate bet outcomes
-					  μ2, # mean of the second gaussian distribution to generate bet outcomes
-					  σ1, # standard deviation of the first gaussian distribution to generate bet outcomes
-					  σ2, # standard deviation of the second gaussian distribution to generate bet outcomes
-					  probability, # probability of success
-					  nObs) # number of observations
-
-	return1=rand(Normal(μ1, σ1), trunc(Int,(nObs*probability))) # draw random bet outcomes from a gaussian distribution
-    return2=rand(Normal(μ2, σ2), trunc(Int,(nObs))-trunc(Int,(nObs*probability))) # draw random bet outcomes from a gaussian distribution
-	
-	returns = append!(return1, return2) # append returns
-	shuffle!(returns) # shuffle returns
-
-	return returns
-end 
-
-function failureProbability(returns, # returns list
-							frequency, # number of bets per year
-							targetSharpeRatio) # target annual Sharpe ratio
-
-    # derive the probability that the strategy may fail
-    rPositive, rNegative = mean(returns[returns .> 0]), mean(returns[returns .<= 0]) # divide returns
-    p = size(returns[returns .> 0], 1)/size(returns, 1) # find success rate
-	thresholdP = impliedPrecision(rNegative, rPositive, frequency, targetSharpeRatio) # calculate success rate threshold
-	risk = cdf(Normal(p, p*(1 - p)), thresholdP) # approximate to bootstrap
-
-	return risk
+function failureProbability(returns, frequency, targetSharpeRatio)
+    rPositive, rNegative = mean(returns[returns .> 0]), mean(returns[returns .<= 0])
+    p = count(returns .> 0) / length(returns)
+    thresholdP = impliedPrecision(rNegative, rPositive, frequency, targetSharpeRatio)
+    risk = cdf(Normal(p, p * (1 - p)), thresholdP)
+    
+    return risk
 end
 
-function calculateStrategyRisk(μ1, # mean of the first gaussian distribution to generate bet outcomes
-							   μ2, # mean of the second gaussian distribution to generate bet outcomes
-							   σ1, # standard deviation of the first gaussian distribution to generate bet outcomes
-							   σ2, # standard deviation of the second gaussian distribution to generate bet outcomes
-							   probability, # probability of success
-							   nObs, # number of observations
-							   frequency, # number of bets per year
-							   targetSharpeRatio) # target annual Sharpe ratio
-							   #1) Parameters
+"""
+Function to calculate the strategy risk in practice.
 
-    returns = mixGaussians(μ1, μ2, σ1, σ2, probability, nObs) # 2) Generate sample from mixture
-
-    probabilityFail = failureProbability(returns, frequency, targetSharpeRatio) # 3) Compute failure probability
-    print("Prob strategy will fail ", probabilityFail) # print result
-
+:param μ1: Mean of the first Gaussian distribution to generate bet outcomes.
+:param μ2: Mean of the second Gaussian distribution to generate bet outcomes.
+:param σ1: Standard deviation of the first Gaussian distribution to generate bet outcomes.
+:param σ2: Standard deviation of the second Gaussian distribution to generate bet outcomes.
+:param probability: Probability of success.
+:param nObs: Number of observations.
+:param frequency: Number of bets per year.
+:param targetSharpeRatio: Target annual Sharpe ratio.
+:return: Probability that the strategy may fail.
+"""
+function calculateStrategyRisk(μ1, μ2, σ1, σ2, probability, nObs, frequency, targetSharpeRatio)
+    returns = mixGaussians(μ1, μ2, σ1, σ2, probability, nObs)
+    probabilityFail = failureProbability(returns, frequency, targetSharpeRatio)
+    
+    println("Probability strategy will fail: ", probabilityFail)
     return probabilityFail
 end
