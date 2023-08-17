894a7b5cd47396c4748973f3e09237729345372d
Seyed Alireza Mousavizade <107984049+salireza-mousavizade@users.noreply.github.com>
2023-08-18 01:22:25 +0330

    reformat

--- src/Utils/RollingFunctions.jl
+++ src/Utils/RollingFunctions.jl
@@ -1,37 +1,46 @@
 module RollingFunctions
 
 export rolling
-
 """
 Calculate rolling computations over two input vectors with a specified window span.
 
+This function applies a given mapping function to rolling windows of specified size over two input vectors.
+The mapping function is applied to each rolling window, and the computed values are returned in an array.
+
 Parameters:
-- mapping: A function that computes a value from the input vectors.
-- vector1: The first input vector.
-- vector2: The second input vector.
-- windowSpan: The size of the rolling window.
+- mapping::Function: The mapping function that computes a value from input vectors.
+- vector1::Vector: The first input vector.
+- vector2::Vector: The second input vector.
+- windowSpan::Int: The size of the rolling window.
 
 Returns:
-An array of computed values with rolling window.
+- result::Vector: An array of computed values using rolling windows.
 """
-function rolling(
-        mapping,
-        vector1,
-        vector2,
-        windowSpan
-    )
-
+function rolling_computations(
+    mapping::Function,
+    vector1::Vector,
+    vector2::Vector,
+    windowSpan::Int
+)::Vector
     @assert length(vector1) == length(vector2)
     n = length(vector1)
-    result = []
+    result = Vector{Any}(undef, n)  # Preallocate result array
+
     for i in 1:n
         rightIndex, leftIndex = i, i - (windowSpan - 1)
-        value = leftIndex ≥ 1 ? mapping(vector1[leftIndex:rightIndex], vector2[leftIndex:rightIndex]) : missing
-        push!(result, value)
+        if leftIndex ≥ 1
+            windowSlice1 = view(vector1, leftIndex:rightIndex)
+            windowSlice2 = view(vector2, leftIndex:rightIndex)
+            value = mapping(windowSlice1, windowSlice2)
+            result[i] = value
+        else
+            result[i] = missing
+        end
     end
 
-    result
+    return result
 end
+
 
 """
 Calculate rolling computations over a single input vector with a specified window span.
@@ -45,11 +54,11 @@ Returns:
 An array of computed values with rolling window.
 """
 function rolling(
-        mapping,
-        vector,
-        windowSpan
-    )
-    
+    mapping,
+    vector,
+    windowSpan
+)
+   
     n = length(vector)
     result = []
     for i in 1:n
--- src/Utils/VectorFunctions.jl
+++ src/Utils/VectorFunctions.jl
@@ -1,31 +1,45 @@
 module VectorFunctions
 
-export differences, vectorToMatrix
+export calculate_differences, vector_to_matrix
 
 """
 Calculate the differences between consecutive elements of a vector.
 
+This function computes the differences between consecutive elements of the input vector.
+The resulting vector has the same length as the input vector, with the first element marked as missing.
+
 Parameters:
-- vector: The input vector.
+- vector::Vector: The input vector.
 
 Returns:
-A new vector with the differences between consecutive elements. The first element is marked as missing.
+- result::Vector: A new vector with the differences between consecutive elements.
+
+Formula:
+.. math::
+    \\text{result}[i] = \\text{vector}[i] - \\text{vector}[i-1]
+
+Where:
+- \\text{result}[i]: The difference between consecutive elements at index i.
+- \\text{vector}[i]: The element at index i of the input vector.
 """
-function differences(vector::Vector)::Vector
-    result = [missing, (vector |> diff)...]
+function calculate_differences(vector::Vector)::Vector
+    result = [missing; diff(vector)]
     result
 end
 
 """
 Convert a vector into a column matrix.
 
+This function converts a given vector into a column matrix.
+Each element of the vector corresponds to a row in the resulting matrix.
+
 Parameters:
-- vector: The input vector.
+- vector::Vector: The input vector.
 
 Returns:
-A column matrix where each element of the vector corresponds to a row in the matrix.
+- matrix::Matrix: A column matrix where each element of the vector corresponds to a row in the matrix.
 """
-function vectorToMatrix(vector::Vector)::Matrix
+function vector_to_matrix(vector::Vector)::Matrix
     reshape(vector, length(vector), 1)
 end
 
